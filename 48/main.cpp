baf9ec9bca64d4a426d2c3356568ab17274ad4bd
igor <igor.240340@gmail.com>
2026-05-09 16:17:06 +0700

    Update an example

--- 48/main.cpp
+++ 48/main.cpp
@@ -3,11 +3,13 @@
 void example1();
 void example2();
 void leaky();
+void malloc_free();
 
 int main() {
 	example1();
 	example2();
 	leaky();
+	malloc_free();
 }
 
 void example1() {
@@ -29,4 +31,22 @@ void example2() {
 void leaky() {
 	new int;
 	std::println("I just leaked an int!");
+}
+
+void malloc_free() {
+	class Foo {
+	public:
+		Foo() { std::println("Foo() called"); };
+		~Foo() { std::println("~Foo() called"); };
+	};
+
+	std::println("\nmalloc()");
+	Foo* foo1{ (Foo*)malloc(sizeof(Foo)) };
+	std::println("free()");
+	free(foo1);
+
+	std::println("\nnew");
+	Foo* foo2{ new Foo };
+	std::println("delete");
+	delete foo2;
 }
