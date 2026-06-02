2c29634dcb273f83d72e5f1e6fc149e6ada9ac05
igor <igor.240340@gmail.com>
2026-06-02 21:04:05 +0700

    Update an example

--- 49/main.cpp
+++ 49/main.cpp
@@ -1,3 +1,14 @@
+#define _CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+	#ifndef DBG_NEW
+		#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+		#define new DBG_NEW
+	#endif
+#endif
+
 #include <print>
 
 class Simple {
@@ -13,6 +24,8 @@ void mem_leak();
 void do_something(Simple*& out_simple_ptr);
 
 int main() {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
 	mem_leak();
 }
 
