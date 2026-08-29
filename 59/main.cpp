a0804d85afcddeead21531b4abcf54db73f9bf7a
igor <igor.240340@gmail.com>
2026-08-29 21:27:18 +0700

    Add example

---
+++ 59/main.cpp
@@ -0,0 +1,35 @@
+import spreadsheet_cell;
+import std;
+
+using namespace std;
+
+int main() {
+	SpreadsheetCell my_cell(4), another_cell(5);
+	SpreadsheetCell a_third_cell(my_cell + another_cell);
+
+	string str("hello");
+	a_third_cell = my_cell + string_view(str);
+
+	a_third_cell = my_cell + 5.6;
+	a_third_cell = my_cell + 4;
+
+	a_third_cell = 5.6 + my_cell;
+	a_third_cell = 4 + my_cell;
+	a_third_cell = 4.5 + 5.5;
+
+	a_third_cell = my_cell - another_cell;
+	a_third_cell = my_cell * another_cell;
+	a_third_cell = my_cell / another_cell;
+
+	try {
+		a_third_cell = my_cell / 0;
+	}
+	catch (const invalid_argument& e) {
+		println("Caught: {}", e.what());
+	}
+
+	a_third_cell -= my_cell;
+	a_third_cell += 5.4;
+	a_third_cell *= my_cell;
+	a_third_cell /= my_cell;
+}
