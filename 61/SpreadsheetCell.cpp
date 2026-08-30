1874766e9f10af90b27c11209051642629a51fa2
igor <igor.240340@gmail.com>
2026-08-30 18:38:01 +0700

    Add example

---
+++ 61/SpreadsheetCell.cpp
@@ -0,0 +1,109 @@
+module spreadsheet_cell;
+
+import std;
+
+using namespace std;
+
+SpreadsheetCell::SpreadsheetCell(double initial_value) : value(initial_value) {}
+
+SpreadsheetCell::SpreadsheetCell(string_view initial_value) : value(string_to_double(initial_value)) {}
+
+void SpreadsheetCell::set(double value) {
+	this->value = value;
+}
+
+double SpreadsheetCell::get_value() const {
+	return value;
+}
+
+void SpreadsheetCell::set(string_view value) {
+	this->value = string_to_double(value);
+}
+
+string SpreadsheetCell::get_string() const {
+	return double_to_string(value);
+}
+
+string SpreadsheetCell::double_to_string(double value) {
+	return to_string(value);
+}
+
+double SpreadsheetCell::string_to_double(string_view value) {
+	double number = 0;
+	from_chars(value.data(), value.data() + value.size(), number);
+	return number;
+}
+
+SpreadsheetCell operator+(const SpreadsheetCell& lhs, const SpreadsheetCell& rhs) {
+	SpreadsheetCell result(lhs);
+	result += rhs;
+	return result;
+
+	//return SpreadsheetCell { lhs.get_value() + rhs.get_value() };
+}
+
+SpreadsheetCell operator-(const SpreadsheetCell& lhs, const SpreadsheetCell& rhs) {
+	SpreadsheetCell result(lhs);
+	result -= rhs;
+	return result;
+
+	//return SpreadsheetCell { lhs.get_value() - rhs.get_value() };
+}
+
+SpreadsheetCell operator*(const SpreadsheetCell& lhs, const SpreadsheetCell& rhs) {
+	SpreadsheetCell result(lhs);
+	result *= rhs;
+	return result;
+
+	//return SpreadsheetCell { lhs.get_value() * rhs.get_value() };
+}
+
+SpreadsheetCell operator/(const SpreadsheetCell& lhs, const SpreadsheetCell& rhs) {
+	SpreadsheetCell result(lhs);
+	result /= rhs;
+	return result;
+
+	//if (rhs.get_value() == 0) {
+	//	throw invalid_argument { "Divide by zero." };
+	//}
+	//return SpreadsheetCell { lhs.get_value() / rhs.get_value() };
+}
+
+SpreadsheetCell& SpreadsheetCell::operator+=(const SpreadsheetCell& rhs) {
+	set(get_value() + rhs.get_value());
+	return *this;
+}
+
+SpreadsheetCell& SpreadsheetCell::operator-=(const SpreadsheetCell& rhs) {
+	set(get_value() - rhs.get_value());
+	return *this;
+}
+
+SpreadsheetCell& SpreadsheetCell::operator*=(const SpreadsheetCell& rhs) {
+	set(get_value() * rhs.get_value());
+	return *this;
+}
+
+SpreadsheetCell& SpreadsheetCell::operator/=(const SpreadsheetCell& rhs) {
+	if (rhs.get_value() == 0) {
+		throw invalid_argument{ "Divide by zero." };
+	}
+	set(get_value() / rhs.get_value());
+	return *this;
+}
+
+bool SpreadsheetCell::operator==(const SpreadsheetCell& rhs) const {
+	return get_value() == rhs.get_value();
+}
+
+bool SpreadsheetCell::operator==(double rhs) const {
+	return get_value() == rhs;
+}
+
+std::partial_ordering SpreadsheetCell::operator<=>(const SpreadsheetCell& rhs) const {
+	return get_value() <=> rhs.get_value();
+}
+
+std::partial_ordering SpreadsheetCell::operator<=>(double rhs) const {
+	return get_value() <=> rhs;
+}
---
+++ 61/SpreadsheetCell.ixx
@@ -0,0 +1,38 @@
+export module spreadsheet_cell;
+
+import std;
+
+export class SpreadsheetCell {
+public:
+	SpreadsheetCell() = default;
+	SpreadsheetCell(double initial_value);
+	SpreadsheetCell(std::string_view initial_value);
+
+	void set(double value);
+	void set(std::string_view value);
+
+	double get_value() const;
+	std::string get_string() const;
+
+	SpreadsheetCell& operator+=(const SpreadsheetCell& rhs);
+	SpreadsheetCell& operator-=(const SpreadsheetCell& rhs);
+	SpreadsheetCell& operator*=(const SpreadsheetCell& rhs);
+	SpreadsheetCell& operator/=(const SpreadsheetCell& rhs);
+
+	[[nodiscard]] bool operator==(const SpreadsheetCell& rhs) const;
+	[[nodiscard]] std::partial_ordering operator<=>(const SpreadsheetCell& rhs) const;
+
+	[[nodiscard]] std::partial_ordering operator<=>(double rhs) const;
+	[[nodiscard]] bool operator==(double rhs) const;
+
+private:
+	static std::string double_to_string(double value);
+	static double string_to_double(std::string_view value);
+
+	double value = 0;
+};
+
+export SpreadsheetCell operator+(const SpreadsheetCell& lhs, const SpreadsheetCell& rhs);
+export SpreadsheetCell operator-(const SpreadsheetCell& lhs, const SpreadsheetCell& rhs);
+export SpreadsheetCell operator*(const SpreadsheetCell& lhs, const SpreadsheetCell& rhs);
+export SpreadsheetCell operator/(const SpreadsheetCell& lhs, const SpreadsheetCell& rhs);
